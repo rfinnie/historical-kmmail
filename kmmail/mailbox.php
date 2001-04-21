6b124eff2f5fbf985c6165072143f8f5ecf72cc7
Ryan Finnie <ryan@finnie.org>
2001-04-21 14:20:06 -0700

    BugID-0000046 - Added images to subject when attachment/forwarded message are present

--- kmmail/mailbox.php
+++ kmmail/mailbox.php
@@ -1,5 +1,5 @@
 <?
-// @(#) $Id: mailbox.php,v 1.11 2001/04/19 06:20:33 ryan Exp $
+// @(#) $Id: mailbox.php,v 1.12 2001/04/21 19:24:17 ryan Exp $
 include_once('include/misc.inc');
 check_cookie($username, $password);
 
@@ -91,10 +91,14 @@ for($i = 0; $i < $count; $i++) {
   ?> 
                 <tr class="<? echo $bgclass; ?>"> 
                   <td> 
-                    <input type="checkbox" name="<? echo ($msgs[$i][deleted] ? 'un' : ''); ?>delete_msg[<? echo $msgs[$i][msgno]; ?>]" />
+                      <input type="checkbox" name="<? echo ($msgs[$i][deleted] ? 'un' : ''); ?>delete_msg[<? echo $msgs[$i][msgno]; ?>]" />
                   </td>
                   <td><a href="message.php?folder=<? echo urlencode($folder); ?>&amp;msgno=<? echo $msgs[$i][msgno]; ?>"><? echo $msgs[$i][from]; ?></a></td>
-                  <td><? echo $msgs[$i][subject]; ?></td>
+                  <td>
+                    <? echo ($msgs[$i]['count_mime']['message/rfc822'] ? '<img src="images/img_envelope.gif" width="15" height="11" alt="*" class="normal" />' : ''); ?>
+                    <? echo ($msgs[$i]['count_disposition']['attachment'] ? '<img src="images/img_file.gif" width="11" height="15" alt="*" class="normal" />' : ''); ?> 
+                    <? echo $msgs[$i][subject]; ?>
+                  </td>
                   <td><? echo km_human_readable_size($msgs[$i][size], 1); ?></td>
                   <td><? echo date("m/d/Y", $msgs[$i][udate]); ?></td>
                 </tr>
