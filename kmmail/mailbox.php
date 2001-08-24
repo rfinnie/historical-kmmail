08af5e8605428a6fa6c6acb79388ccb5634b2c04
Ryan Finnie <ryan@finnie.org>
2001-08-23 21:30:59 -0700

    Added ability to page through mailboxes, and see replied messages

--- kmmail/mailbox.php
+++ kmmail/mailbox.php
@@ -1,5 +1,5 @@
 <?
-// @(#) $Id: mailbox.php,v 1.14 2001/04/23 02:02:24 ryan Exp $
+// @(#) $Id: mailbox.php,v 1.15 2001/04/23 02:55:58 ryan Exp $
 include_once('include/misc.inc');
 check_cookie($username, $password);
 
@@ -25,10 +25,12 @@ if($action_delete) {
   $imap->move_messages($folder, $move_msg, $move_folder);
   header("Location: mailbox.php?folder=$folder");
 }
-$msgs = $imap->retrieve_message_list();
+$offset = ($offset ? $offset : 1);
+$return = 20;
+$count = $imap->retrieve_num_messages();
+$msgs = $imap->retrieve_message_list($offset, $return);
 $boxes = $imap->retrieve_mailboxes_short();
 $imap->disconnect();
-$count = count($msgs);
   ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -69,6 +71,7 @@ $count = count($msgs);
               </tr>
             </table>
             <p /> 
+            <p /> 
             <form method="post" action="<? echo $PHP_SELF; ?>">
               <input type="hidden" name="folder" value="<? echo $folder; ?>" />
               <table width="100%" border="0" cellpadding="2" cellspacing="1" class="backblack">
@@ -80,7 +83,7 @@ $count = count($msgs);
                   <td><b>Date</b></td>
                 </tr>
                 <?
-for($i = 0; $i < $count; $i++) {
+for($i = 0; $i < count($msgs); $i++) {
   if($msgs[$i][deleted]) {
     $bgclass = "messagelist-deleted";
   } elseif($config['is_pop3']) {
@@ -99,6 +102,7 @@ for($i = 0; $i < $count; $i++) {
                   <td> <? echo ($msgs[$i]['count_mime']['message/rfc822'] ? '<img src="images/img_envelope.gif" width="15" height="11" alt="*" class="normal" />' : ''); ?> 
                     <? echo ($msgs[$i]['count_disposition']['attachment'] ? '<img src="images/img_file.gif" width="11" height="15" alt="*" class="normal" />' : ''); ?> 
                     <? echo ($msgs[$i]['count_mime']['text/html'] ? '<img src="images/img_world.gif" width="13" height="13" alt="*" class="normal" />' : ''); ?> 
+                    <? echo ($msgs[$i]['replied'] ? '<img src="images/img_replied.gif" width="7" height="10" alt="*" class="normal" />' : ''); ?> 
                     <? echo $msgs[$i][subject]; ?> </td>
                   <td><? echo km_human_readable_size($msgs[$i][size], 1); ?></td>
                   <td><? echo date("m/d/Y", $msgs[$i][udate]); ?></td>
@@ -113,6 +117,37 @@ if($count == 0) {
                 <?
 } else {
   ?> 
+                <tr class="messagelist-read">
+                  <td colspan="5">
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+<tr>
+<td align="left">
+<?
+if($offset > 1) {
+  $newoffset = (($offset - $return) < 1 ? 0 : ($offset - $return));
+  ?>
+<a href="mailbox.php?folder=<? echo $folder; ?>&amp;offset=<? echo $newoffset; ?>">&lt;&lt;</a> 
+  <?
+}
+?>
+</td>
+<td align="center">
+Displaying messages <? echo $offset; ?>-<? echo (($offset + $return) > $count ? $count : ($offset + $return - 1)); ?> of <? echo $count; ?>
+</td>
+<td align="right">
+<?
+if(($offset + $return) < $count) {
+  $newoffset = $offset + $return;
+  ?>
+<a href="mailbox.php?folder=<? echo $folder; ?>&amp;offset=<? echo $newoffset; ?>">&gt;&gt;</a> 
+  <?
+}
+?>
+</td>
+</tr>
+</table>
+</td>
+                </tr>
                 <tr class="messagelist-read"> 
                   <td colspan="5"> 
                     <input type="submit" name="action_delete" value="Delete" />
